a397a68e96ccd438f60fdc121336d2c7400049d6
kyoulau <laucosan05@gmail.com>
2024-04-07 12:46:14 -0300

    footer e home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+// fazer componente do card e personalizar a pagina
+}
