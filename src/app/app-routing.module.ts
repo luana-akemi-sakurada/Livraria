631399a5a1bbae568a33a4df872b0fe0b1efade8
andretini <andrealijaramaosagostini@gmail.com>
2024-04-07 13:25:56 -0300

    adição do component perfil e ajustes nas routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:NavbarComponent, children:[
+    {path:'', component:FooterComponent, children:[
+      {path:'perfil', component:PerfilComponent}
+    ]}
+  ]},
+  {path:'home', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
