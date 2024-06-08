b62ce7b85022ff2f2658d6308bc7249ec7770126
luana-akemi-sakurada <111025797+luana-akemi-sakurada@users.noreply.github.com>
2024-06-08 16:57:10 -0300

    bd de explorar

--- src/app/criar-livro/service/livro.service.ts
+++ src/app/criar-livro/service/livro.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@angular/core";
 import { Livro } from "../model/livro.model";
 import { AngularFireDatabase } from "@angular/fire/compat/database";
 import { AngularFireStorage } from "@angular/fire/compat/storage";
+import {map} from "rxjs";
+
+
 
 @Injectable({
     providedIn: 'root'
@@ -19,5 +22,16 @@ export class LivroService {
     
     salvar(livro: Livro){
         return this.db.list('livros').push(livro);
+    }
+
+    listar(){
+        return this.db.list('livros').snapshotChanges().pipe(
+            map(changes => {
+                return changes.map(c => ({ 
+                    key : c.payload.key,
+                    ...c.payload.val() as Livro
+                }))
+            })
+        )
     }
 }
--- src/app/explorar/explorar.component.ts
+++ src/app/explorar/explorar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LivroService } from '../criar-livro/service/livro.service';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-explorar',
   templateUrl: './explorar.component.html',
   styleUrl: './explorar.component.css'
 })
-export class ExplorarComponent {
+export class ExplorarComponent implements OnInit {
+
+  public livros: any;
+
+  constructor(
+    private livroService: LivroService, 
+    private router: Router) { }
+
+  ngOnInit(): void {
+      this.livroService.listar().subscribe(livros=>{
+        console.log(livros);
+        this.livros = livros;
+      }); 
+  }
 
 }
