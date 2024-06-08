7e30f8861bb6468cae4dda7422b96f88d5e7c202
André Alija Ramos Agostini <andrealijaramosagostini@gmail.com>
2024-06-08 11:19:19 -0300

    Back pagina criar conta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
