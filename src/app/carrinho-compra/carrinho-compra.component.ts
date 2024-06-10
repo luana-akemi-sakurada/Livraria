5dd99c1cd9a29443aa0e942421c2aa9220712b1d
André Alija Ramos Agostini <andrealijaramosagostini@gmail.com>
2024-06-09 21:07:57 -0300

    carrinho ajuda

--- src/app/carrinho-compra/carrinho-compra.component.ts
+++ src/app/carrinho-compra/carrinho-compra.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from './service/carrinho.service';
+import { Livro } from '../criar-livro/model/livro.model';
+import { LivroService } from '../criar-livro/service/livro.service';
 
 @Component({
   selector: 'app-carrinho-compra',
   templateUrl: './carrinho-compra.component.html',
   styleUrl: './carrinho-compra.component.css'
 })
-export class CarrinhoCompraComponent {
+export class CarrinhoCompraComponent implements OnInit{
+  constructor(private carrinhoService: CarrinhoService, private livroService: LivroService){
+
+  }
+  public carrinho: any;
+  public livros: any;
+
+  ngOnInit(): void {
+
+    this.carrinhoService.listar().subscribe(cart=>{
+      console.log(cart);
+      this.carrinho = cart;
+    }); 
+    
+    this.livroService.listar().subscribe(livro=>{
+      console.log(livro);
+      this.livros = livro;
+    }); 
+    
+  }
+  
 
 }
--- src/app/explorar/explorar.component.ts
+++ src/app/explorar/explorar.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { LivroService } from '../criar-livro/service/livro.service';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SessaoService } from '../Sessao/sessao.service';
+import { Carrinho } from '../carrinho-compra/model/carrinho.model';
+import { CarrinhoService } from '../carrinho-compra/service/carrinho.service';
 
 
 
@@ -18,13 +20,22 @@ export class ExplorarComponent implements OnInit {
   constructor(
     private livroService: LivroService, 
     private router: Router,
-    private service: SessaoService ) { }
+    private sessao: SessaoService,
+    private carrinhoService: CarrinhoService) { }
 
   ngOnInit(): void {
       this.livroService.listar().subscribe(livros=>{
         console.log(livros);
         this.livros = livros;
       }); 
+  }
+
+  addToCart(nome: string): void{
+    let cart = new Carrinho()
+    cart.keyUser = this.sessao.getUsuario().email
+    cart.keyLivro = nome
+    cart.Quantidade = "1"
+    this.carrinhoService.salvar(cart)
   }
 
 }
